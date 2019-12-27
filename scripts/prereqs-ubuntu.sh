53aa253e0d12cf27f8fc3bee3ac5bd79bac52301
Devilpacks <mangyr97@mail.ru>
2019-12-27 11:06:28 +0300

    Add files via upload

--- scripts/prereqs-ubuntu.sh
+++ scripts/prereqs-ubuntu.sh
@@ -67,8 +67,8 @@ export NVM_DIR="${HOME}/.nvm"
 
 # Install node
 echo "# Installing nodeJS"
-nvm install 8
-nvm use 8
+nvm install 12
+nvm use 12
 
 # Ensure that CA certificates are installed
 sudo apt-get -y install apt-transport-https ca-certificates
@@ -101,7 +101,7 @@ sudo usermod -aG docker $(whoami)
 
 # Install docker compose
 echo "# Installing Docker-Compose"
-sudo curl -L "https://github.com/docker/compose/releases/download/1.13.0/docker-compose-$(uname -s)-$(uname -m)" \
+sudo curl -L "https://github.com/docker/compose/releases/download/1.17.1/docker-compose-$(uname -s)-$(uname -m)" \
     -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 
